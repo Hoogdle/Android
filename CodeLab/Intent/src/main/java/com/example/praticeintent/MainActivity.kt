d456a73465595be74a5f9778ad5b82c64ad7dc8b
Kim Taeyeong <xodud7737@gmail.com>
2025-03-12 21:05:08 +0900

    _

---
+++ CodeLab/Intent/src/main/java/com/example/praticeintent/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.praticeintent
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.praticeintent.ui.theme.PraticeIntentTheme
+import androidx.core.net.toUri
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // what is the problem!!!
+        setContent {
+            Display { url ->
+                val uri = Uri.parse(url)
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
+        }
+    }
+}
+
+@Composable
+fun Display(useIntent: (String) -> Unit){
+
+    val text = remember{mutableStateOf("")}
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        TextField(
+            value = text.value,
+            onValueChange = {text.value = it},
+            label = {Text(text = "type any url")}
+        )
+        Button(
+            onClick = {useIntent(text.value)}
+        ) {
+            Text(text = "enter")
+        }
+    }
+}
